0aa6c438663469b068223e8f10064f3d1babded3
Kishor <dhokade5784@gmail.com>
2023-12-06 11:53:30 +0530

    added changes for data

--- app.js
+++ app.js
@@ -107,6 +107,108 @@ app.delete('/deleteUser/:id', async (req, res) => {
 });
 
 
+app.post("/populationInfo", async (request, response) => {
+    const body = request.body;
+    const userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        }, 
+        gender: String,
+        age: Number,
+        religion: String,
+        caste: String,
+        maritalStatus: Number,
+        language: String,
+        address: String,
+        city: String,
+        pincode: Number,
+        dist: String,
+        state: String,
+        country: String,
+        housingType: String,
+        phoneNo: Number,
+        HighestEducation: String,
+        schoolCollegeName: String,
+        occupation: String,
+        partTimeFullTime: String,
+        comapany: String,
+        annualIncome: Number,
+        medicalCondition: String,
+        noOfFamilyMembers: String,
+        familyIncome: Number,
+        annualIncomeOfFamily: Number,
+        voterId: Number,
+        rationCard: Number,
+        panCard: Number,
+        aadharCard: Number,
+        nameOfOfficer: String,
+        date: Date,
+    })
+    
+    const newElectionInfoData = new mongoose.model('ElectionInfo', userSchema)
+
+    // const newUser = new newUserData({
+    //     name: body.name,
+    //     address: body.address,
+    //     rollNo: body.rollNo,
+    //     author: body.author
+    // })
+
+    const newElectionInfo = new newElectionInfoData({
+        name: body.personalInfo.name,
+        gender: body.personalInfo.name,
+        age: body.personalInfo.age,
+        religion: body.personalInfo.religion,
+        caste: body.personalInfo.caste,
+        maritalStatus: body.personalInfo.maritalStatus,
+        language: body.personalInfo.language,
+        address: body.contactDetails.address,
+        city: body.contactDetails.city,
+        pincode: body.contactDetails.pincode,
+        dist: body.contactDetails.dist,
+        state: body.contactDetails.state,
+        country: body.contactDetails.country,
+        housingType: body.contactDetails.housingType,
+        phoneNo: body.contactDetails.phoneno,
+        highestEducation: body.educational.highestEducation,
+        schoolCollegeName: body.educational.schoolCollegeName,
+        occupation: body.occupational.occupation,
+        partTimeFullTime: body.occupational.partTimeFullTime,
+        company: body.occupational.company,
+        annualIncome: body.occupational.annualIncome,
+        medicalCondition: body.health.medicalCondition,
+        noOfFamilyMembers: body.family.noOfFamilyMembers,
+        familyIncome: body.family.familyIncome,
+        annualIncomeOfFamily: body.family.$oidannualIncomeOfFamily,
+        voterId: body.voterId,
+        rationCard: body.rationCard,
+        panCard: body.panCard,
+        aadharCard: body.aadharCard,
+        nameOfOfficer: body.nameOfOfficer,
+        date: body.date,
+    })
+
+    let status = '';
+    await newElectionInfo.save().then(() => {
+        status = {
+            message: "Election Info Added Successfully",
+            code:200
+        };
+            console.log("Saved Successfully");
+    }, error => {
+        status = error;
+    });
+
+    if (status) {
+        response.send(status);
+    } else {
+        throw status;
+    }
+
+});
+
+
     
 
 
@@ -166,3 +268,49 @@ app.delete('/deleteUser/:id', async (req, res) => {
 //     // })
   
 // });
+
+// const a = {
+//     personalInfo: {
+//         "name": "Kiran",
+//         "gender": "Male",
+//         "age": 20,
+//         "religion": "hindu",
+//         "caste": "maratha",
+//         "maritalStatus": "married",
+//         "language": "marathi",
+//     },
+//     contactDetails: {
+//         address: "UK",
+//         city: "Pune",
+//         pincode: body.contactDetails.pincode,
+//         dist: body.contactDetails.dist,
+//         state: body.contactDetails.state,
+//         country: body.contactDetails.country,
+//         housingType: body.contactDetails.housingType,
+//         phoneNo: body.contactDetails.phoneno,
+//     },
+//     education: {
+//         highestEducation: body.educational.highestEducation,
+//         schoolCollegeName: body.educational.schoolCollegeName,
+//     },
+//     occupational: {
+//         occupation: body.occupational.occupation,
+//         partTimeFullTime: body.occupational.partTimeFullTime,
+//         company: body.occupational.company,
+//         annualIncome: body.occupational.annualIncome,
+//     },
+//     health: {
+//         "medicalCondition": "good",
+//     },
+//     family: {
+//         "noOfFamilyMembers": 5,
+//         "familyIncome": 10000,
+//         "annualIncomeOfFamily": 50000
+//     },
+//    "voterId": 123,
+//     "rationCard": 456,
+//     "panCard": 123,
+//     "aadharCard": 123,
+//     "nameOfOfficer": "XYZ",
+//     "date": 12/12/2023
+// }
