6b77cbd1680d5c87b813498986896e7f5815f2fd
Rutuja341 <rutujak341@gmail.com>
2023-12-10 12:23:24 +0530

    new api added

--- app.js
+++ app.js
@@ -438,3 +438,155 @@ const a = {
     "nameOfOfficer": "XYZ",
     "date": 12/12/2023
 }
+app.get('/getCarsOptions',async (req, res)=>{
+    const result = {
+        brandList : [
+            {brand: 'MarutiSuzuki',
+              models: ['Swift', 'Baleno', 'Alto', 'Dzire', 'Ciaz']},
+            {brand: 'Toyota',
+              models: ['Corolla', 'Camry', 'Fortuner', 'Innova', 'Yaris']},
+            {brand: 'Kia',
+              models: ['Seltos', 'Sonet', 'Carnival', 'Sportage']},
+            {brand: 'Hyundai',
+              models: ['i20', 'Creta', 'Venue', 'Verna', 'Tucson']},
+            {brand: 'Honda',
+              models: ['Civic', 'Accord', 'CR-V', 'City', 'Amaze']},
+            {brand: 'Tata',
+              models: ['Tiago', 'Nexon', 'Harrier', 'Altroz', 'Safari']},
+            {brand: 'Mahindra',
+              models: ['Scorpio', 'XUV500', 'Thar', 'Bolero', 'KUV100']},
+            {brand: 'Ford',
+              models: ['EcoSport', 'Figo', 'Endeavour', 'Aspire', 'Mustang']},
+            {brand: 'Renault',
+              models: ['Kwid', 'Duster', 'Triber', 'Captur']},
+            {brand: 'Volkswagen',
+              models: ['Polo', 'Vento', 'Tiguan', 'Passat']},
+            {brand: 'Chevrolet',
+              models: ['Cruze', 'Beat', 'Trailblazer', 'Sail']},
+            {brand: 'Fiat',
+              models: ['Punto', 'Linea', '500']}
+          ],
+        makeYear : [2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011],
+        variants : ['Petrol','Diesel','CNG'],
+        kmDriven : ["0 - 10,000 Km","10,000 - 20,000 km","20,000 - 30,000 Km","30,000 - 40,000 Km","40,000 - 50,000 Km",
+        "Above 50,000 km"],
+        features : ["Infotainment system","Airbags","steering mounted controls","Parking sensors","Alloy wheels",
+        "Push button start","Sunroof/moonroof"],
+        transmissions : ["manual","automatic"],
+        bodyTypes : ["Hatchback","Sedan","suv"],
+        colors : ["Silver","white","Red","Blue","Brown","Orange","Black","Yellow","Green","Purple"],
+        seats : ["4 Seater","5 Seater","6 Seater","7 Seater"],
+        owners : ["1st owner","2nd owner","3rd owner"],
+        states : [{state : 'Maharashtra', codes : ['MH-12','MH-14','MH-01','MH-13']},
+        {state : 'Gujrat', codes : ['GJ-01','GJ-05','GJ-06']},
+        {state : 'Karnataka', codes : ['Ka-01','KA-02']},
+        {state : 'Punjab', codes : ['PB-01','PB-02']}]
+    }
+    let status = {
+        data: result,
+        code:200
+    };
+    console.log("getCarsOptions", result)
+    res.send(status)
+});
+
+app.post('/postCarInfo',async (request, response)=>{
+    const body = request.body;
+    const carSchema = new mongoose.Schema({
+        brand : String,
+        model : String,
+        makeYear : Number,
+        variant : String,
+        kmDriven : String,
+        features : String,
+        transmission : String,
+        bodyType : String,
+        color : String,
+        seats : Number,
+        owner : String,
+        state : String,
+        stateCode : String,
+        city : String,
+        price : Number        
+    })
+    var newCarData;
+
+    if (mongoose.models.CarData){
+        newCarData = mongoose.model('CarData');
+    }else {
+        newCarData = mongoose.model('CarData',carSchema);
+    }
+
+
+    const newCarInfo = new newCarData({
+        brand : body.brand,
+        model : body.model,
+        makeYear : body.makeYear,
+        variant : body.variant,
+        kmDriven : body.kmDriven,
+        features : body.features,
+        transmission : body.transmission,
+        bodyType : body.bodyType,
+        color : body.color,
+        seats : body.seats,
+        owner : body.owner,
+        state : body.state,
+        stateCode : body.stateCode,
+        city : body.city,
+        price : body.price 
+    })
+
+    let status = '';
+    await newCarInfo.save().then(() => {
+        status = {
+            message : "Car Info added successfully",
+            code : 200
+        };
+        console.log("Saved successfully");
+    }, error => {
+        status = error;
+    });
+
+    if(status) {
+        response.send(status);
+    } else {
+        throw status;
+    }
+});
+
+app.get('/getCarInfo', async (req, res) => {
+    const carSchema = new mongoose.Schema({
+        id : Object,
+        brand : String,
+        model : String,
+        makeYear : Number,
+        variant : String,
+        kmDriven : String,
+        features : String,
+        transmission : String,
+        bodyType : String,
+        color : String,
+        seats : Number,
+        owner : String,
+        state : String,
+        stateCode : String,
+        city : String,
+        price : Number
+    })
+    var carList;
+
+    if (mongoose.models.CarData){
+        carList = mongoose.model('CarData');
+    } else {
+        carList = mongoose.model('CarData',carSchema);
+    }
+
+    const result = await carList.find({});
+
+    let status = {
+        data : result,
+        code : 200
+    };
+    console.log("CarList",result);
+    res.send(status);
+});
